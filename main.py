8f71ea8bb231a4009d1856cd4bdcee4f2f580c79
Adam Paganelli <adam@webaholics.co>
2025-02-06 13:55:58 -0700

    Black screen setup

---
+++ main.py
@@ -0,0 +1,25 @@
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))  # black color in RGB
+        pygame.display.flip()
+
+
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
